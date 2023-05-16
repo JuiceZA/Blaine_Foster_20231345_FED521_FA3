eb7112cc2cee594ca58251a6b950d8e28b8ebb84
blaine <blainefoster12@gmail.com>
2023-05-16 18:27:25 +0200

    First commit

---
+++ code/app.js
@@ -0,0 +1,18 @@
+let i = 0;
+let images = [];
+var time = 3000;
+
+images[0]='assets/image1.jpg';
+images[1]='assets/image2.jpg';
+function changeImg(){
+    document.slide.src = images[i];
+    if (i < images.length - 1){
+        i++;
+    } else{
+        i = 0;
+    }
+
+    setTimeout("changeImg()",time);
+}
+
+window.onload = changeImg;
